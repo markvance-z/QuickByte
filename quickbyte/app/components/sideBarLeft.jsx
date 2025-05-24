a1df49899aae460cfb0addba737addabf625dd4f
markvance-z <markvance@oakland.edu>
2025-05-24 13:59:56 -0400

    side bar shows user saved recipes

--- quickbyte/app/components/sideBarLeft.jsx
+++ quickbyte/app/components/sideBarLeft.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import supabase from '../../lib/supabaseClient';
 
-export default function SideBarLeft() {
+
+export default async function SideBarLeft() {
+    const { data: user_saved } = await supabase.from("user_saved").select('*,recipes(*)');
+    
+    const { data: recipes } = await supabase.from("recipes").select();
+
     return <div>
-        <ul>
-            
-        </ul>
+            <span>▽ Uncategorized</span>
+            <ul>
+                {user_saved.map((user_save) => (
+                    <li key={user_save.recipe_id}>{user_save.recipes.title}</li>
+                ))}
+            </ul>
         </div>
-
-
 }
