2e27eb45f39d2e5a5986fa90cbde1732a069235a
markvance-z <markvance@oakland.edu>
2025-05-24 12:45:19 -0400

    change header image

--- quickbyte/app/components/header.jsx
+++ quickbyte/app/components/header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Image from "next/image";
 
+const width = 200;
+const height = width/2.048;
+
 export default function Header() {
     return <header className="header">
-        <span className="title">QuickByte</span><Image 
-            src="/cat.png"
-            width={40}
-            height={40}
-            alt="cat"
-        />
+        <span> <Image 
+            src="/logo-name.png"
+            width={width}
+            height={height}
+            alt="QuickByte"
+        /></span>
     </header>
 
 }
