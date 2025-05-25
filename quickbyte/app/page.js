5ab5d3d7942bfe74490c8db38c09622a9d717b6c
laggyserver <awilliams9@oakland.edu>
2025-05-25 19:35:24 -0400

    removed dead code

---
+++ quickbyte/app/page.js
@@ -0,0 +1,27 @@
+'use client';
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { use, useEffect, useState } from "react";
+import supabase from '../lib/supabaseClient';
+
+export default function Home() {  
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1> Welcome to QuickByte! </h1>
+        <p> Please sign up or log in to continue. </p>         
+        <div className={styles.buttons}>
+          <Link href="/signup">
+            <button className={styles.button}>Sign Up</button>
+          </Link>
+          <Link href="/login">
+            <button className={styles.button}>Log In</button>
+          </Link>        
+        </div>
+      </main>
+      
+    </div>
+  );
+}
