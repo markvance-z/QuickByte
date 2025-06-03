73b1ca94570f3cfb399094b1990e3ec39c9b7277
Haadia Siddiqui <haadiasiddiqui@Haadias-MacBook-Pro.local>
2025-06-02 23:44:43 -0400

    Updated homepage layout and styling

--- quickbyte/app/page.jsx
+++ quickbyte/app/page.jsx
@@ -6,21 +6,25 @@ import Link from "next/link";
 import { use, useEffect, useState } from "react";
 import supabase from '../lib/supabaseClient';
 
-export default function Home() {  
+export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <h1> Welcome to QuickByte! </h1>
-        <p> Please sign up or log in to continue. </p>         
-        <div className={styles.buttons}>
-          <Link href="/signup">
-            <button className={styles.button}>Sign Up</button>
-          </Link>
-          <Link href="/login">
-            <button className={styles.button}>Log In</button>
-          </Link>      
-        </div>
-      </main>      
+      
+      <div className={styles.content}>
+        <main className={styles.main}>
+          <h1>Welcome to QuickByte!</h1>
+          <p>Please sign up or log in to continue.</p>
+          <div className={styles.buttons}>
+            <Link href="/signup">
+              <button className={styles.button}>Sign Up</button>
+            </Link>
+            <Link href="/login">
+              <button className={styles.button}>Log In</button>
+            </Link>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
+
