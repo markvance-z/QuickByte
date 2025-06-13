678edc4e0519f3d0bc030f4335ee3af1e83c4f8b
markvance-z <markvance@oakland.edu>
2025-06-12 22:03:56 -0400

    add testing folder

--- quickbyte/testing/dashboard.test.js
+++ quickbyte/testing/dashboard.test.js
@@ -1,5 +1,15 @@
 'use strict';
 
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import Dashboard from '@/app/dashboard/page';
 
+describe('Dashboard', () => {
+    it('renders a heading', () => {
+            render(<Dashboard/>)
+
+    const heading = screen.getByRole('heading', { level: 1 })
+
+    expect(heading).toBeInTheDocument()
+})
+
+})
