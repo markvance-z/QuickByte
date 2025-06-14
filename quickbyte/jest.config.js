065870f8dc3ae3c48b182222fb8d0143d029b43c
laggyserver <awilliams9@oakland.edu>
2025-06-13 20:04:36 -0400

    fixed build issues

--- quickbyte/jest.config.js
+++ quickbyte/jest.config.js
@@ -3,15 +3,13 @@ module.exports = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './testing/babel.jest.config.js' }],
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleFileExtensions: ['js', 'jsx'],
-
-   moduleNameMapper: {
+  moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
   },
-
   moduleDirectories: ['node_modules', '<rootDir>'],
   modulePaths: ['<rootDir>'],
 };
